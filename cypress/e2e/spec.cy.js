0f6a3ef3021f71d8ded48de67eacde2a4162af1f
Anastasia Krapivina <krapivina934@gmail.com>
2023-11-04 11:49:16 +0700

    add commands

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,79 +1,18 @@
 describe("testing api", () => {
   it("creating a user", () => {
-    cy.request("POST", "https://petstore.swagger.io/v2/user", {
-      id: 237,
-      username: "name1",
-      userStatus: 2,
-    }).then((response) => {
-      expect(response.body).be.eqls({
-        code: 200,
-        type: "unknown",
-        message: "237",
-      });
-    });
-    cy.request(`https://petstore.swagger.io/v2/user/name1`).then((response) => {
-      expect(response.body).be.eqls({
-        id: 237,
-        username: "name1",
-        userStatus: 2,
-      });
-    });
+    cy.postApi("237", "name1");
+    cy.getApi(237, "name1");
   });
 
   it("user edit", () => {
-    cy.request("POST", "https://petstore.swagger.io/v2/user", {
-      id: 238,
-      username: "name2",
-      userStatus: 2,
-    });
-    cy.request("PUT", "https://petstore.swagger.io/v2/user/name2", {
-      id: 238,
-      username: "name3",
-      userStatus: 2,
-    }).then((response) => {
-      expect(response.body).be.eqls({
-        code: 200,
-        type: "unknown",
-        message: "238",
-      });
-    });
-    cy.request("https://petstore.swagger.io/v2/user/name3").then((response) => {
-      expect(response.body).be.eqls({
-        id: 238,
-        username: "name3",
-        userStatus: 2,
-      });
-    });
+    cy.postApi(237, "name2");
+    cy.putApi(238, "name2", "name3");
+    cy.getApi(238, "name3");
   });
 
   it("user delete", () => {
-    cy.request("POST", "https://petstore.swagger.io/v2/user", {
-      id: 240,
-      username: "name5",
-      userStatus: 2,
-    }).then((response) => {
-      expect(response.body).be.eqls({
-        code: 200,
-        type: "unknown",
-        message: "240",
-      });
-    });
-    cy.request("DELETE", "https://petstore.swagger.io/v2/user/name5", {
-      id: 240,
-      username: "name5",
-      userStatus: 2,
-    }).then((response) => {
-      expect(response.body).be.eqls({
-        code: 200,
-        type: "unknown",
-        message: "name5",
-      });
-    });
-    cy.request({
-      url: "https://petstore.swagger.io/v2/user/name5",
-      failOnStatusCode: false,
-    }).then((response) => {
-      expect(response.status).be.eq(404);
-    });
+    cy.postApi(240, "name5");
+    cy.deleteApi(240, "name5");
+    cy.getAfterDelete("name5");
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,65 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import apiData from "../fixtures/apiData.json";
+
+Cypress.Commands.add("postApi", (idNamber, username) => {
+  cy.request("POST", apiData.url, {
+    id: idNamber,
+    username: username,
+    userStatus: 2,
+  }).then((response) => {
+    expect(response.body).be.eqls({
+      code: 200,
+      type: "unknown",
+      message: String(idNamber),
+    });
+  });
+});
+
+Cypress.Commands.add("getApi", (id, username) => {
+  cy.request(apiData.url + "/" + username).then((response) => {
+    expect(response.body).be.eqls({
+      id: id,
+      username: username,
+      userStatus: 2,
+    });
+  });
+});
+
+Cypress.Commands.add("putApi", (idNamber, username, usernameNew) => {
+  cy.request("PUT", apiData.url + "/" + username, {
+    id: idNamber,
+    username: usernameNew,
+    userStatus: 2,
+  }).then((response) => {
+    expect(response.body).be.eqls({
+      code: 200,
+      type: "unknown",
+      message: String(idNamber),
+    });
+  });
+});
+
+Cypress.Commands.add("deleteApi", (idNamber, username) => {
+  cy.request("DELETE", apiData.url + "/" + username, {
+    id: idNamber,
+    username: username,
+    userStatus: 2,
+  }).then((response) => {
+    expect(response.body).be.eqls({
+      code: 200,
+      type: "unknown",
+      message: username,
+    });
+  });
+});
+
+Cypress.Commands.add("getAfterDelete", (username) => {
+  cy.request({
+    url: apiData.url + "/" + username,
+    failOnStatusCode: false,
+  }).then((response) => {
+    expect(response.status).be.eq(404);
+  });
+});
